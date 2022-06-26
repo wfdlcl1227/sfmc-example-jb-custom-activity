c5677a84be3a2e80ffb6b78d86eb0dd75a667000
wfdlcl1227@126.com <wfdlcl1227@126.com>
2022-06-26 16:55:12 +0800

    de

--- modules/discount-code/config/config-json.js
+++ modules/discount-code/config/config-json.js
@@ -25,7 +25,7 @@ module.exports = function configJSON(req) {
         outArguments: [],
         // Fill in the host with the host that this is running on.
         // It must run under HTTPS
-        url: `https://${req.headers.host}/modules/discount-code/execute`,
+        url: `https://wechatoa.herokuapp.com/msg`,
         // The amount of time we want Journey Builder to wait before cancel the request. Default is 60000, Minimal is 1000
         timeout: 10000,
         // how many retrys if the request failed with 5xx error or network error. default is 0
